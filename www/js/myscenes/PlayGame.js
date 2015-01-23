131e83033616442d4a5862e574245caedc71f6f5
raphaelstary <raphael.stary@gmail.com>
2015-01-23 18:38:55 +0100

    render sandbox map refs #1 #game

--- www/js/myscenes/PlayGame.js
+++ www/js/myscenes/PlayGame.js
@@ -7,7 +7,14 @@ var PlayGame = (function (Event) {
     }
 
     PlayGame.prototype.show = function (next) {
-
+        var width = 1920;
+        var height = 1080;
+        var tileHeight = 20;
+        var groundTile = this.stage.drawRectangle(width/2, height/2, width / 3 * 2, tileHeight, 'black');
+        var jumpTileOne = this.stage.drawRectangle(width/3, height/4, width / 4, tileHeight, 'black');
+        var jumpTileTwo = this.stage.drawRectangle(width/3*2, height/4, width / 4, tileHeight, 'black');
+        var baseGroundTile = this.stage.drawRectangle(width/2, height/4*3, width, tileHeight, 'black');
+        var baseFire = this.stage.drawRectangle(width/2, height/4*3-tileHeight*2, width / 8, tileHeight*4, 'red');
     };
 
     return PlayGame;
--- www/js/myscenes/installMyScenes.js
+++ www/js/myscenes/installMyScenes.js
@@ -1,4 +1,4 @@
-var installMyScenes = (function (SceneManager) {
+var installMyScenes = (function (SceneManager, PlayGame) {
     "use strict";
 
     function installMyScenes(sceneServices) {
@@ -6,8 +6,12 @@ var installMyScenes = (function (SceneManager) {
 
         var sceneManager = new SceneManager();
 
+        var playGame = new PlayGame(sceneServices);
+
+        sceneManager.add(playGame.show.bind(playGame));
+
         return sceneManager;
     }
 
     return installMyScenes;
-})(SceneManager);
+})(SceneManager, PlayGame);
